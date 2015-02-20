27720e2ee428e2919b1b737a45645bab5785cd29
Dex Fitch <fitch.dexter@gmail.com>
2015-02-20 15:41:38 -0800

    Add base HTML and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 var Pizza = {
   price: 0,
   size: 0,
+  crust: "classic",
+  glutenFree: false,
+  cutStyle: "wheel",
   toppings: [],
   slices: 0,
 
   pizzaAnalyzer: function() {
-    this.slices = this.size - 4;
+    if (this.cutStyle === "wheel") {
+      this.slices = this.size - 4;
+    } else if (this.cutStyle === "slim") {
+      this.slices = (this.size -4) * 2;
+    } else if (this.cutStyle === "grid") {
+      if (this.size < 12) {
+        this.slices += 9;
+      } else if (this.size < 16) {
+        this.slices += 16;
+      } else {
+        this.slices += 25;
+      }
+    }
     this.price = this.size - 2;
   }
 };
@@ -15,14 +30,13 @@ var Customer = {
   total: 0,
 
   totalCalc: function() {
-    this.pizzas.forEach(pizza) {
+    this.pizzas.forEach(function(pizza) {
       this.total += pizza.price;
-    };
+    });
   }
 };
 
 $(document).ready(function() {
   var newCustomer = Object.create(Customer);
   var newPizza = Object.create(Pizza);
-
-})
+});
